3104532af59106ae6c2131a1d2e1e794bb0bafd1
Joker Apple <734719526@qq.com>
2021-02-25 15:50:50 +0800

    项目初次提交

---
+++ Idle Plunder War/Assets/Scrpits/Utils/RayUtil.cs
@@ -0,0 +1,17 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class RayUtil
+{
+ 
+    /// <summary>
+    /// 屏幕点击射线检测
+    /// </summary>
+    /// <param name="isCollider"></param>
+    /// <param name="hit"></param>
+    public static void RayToScreenPoint(out bool isCollider,out RaycastHit hit)
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        isCollider = Physics.Raycast(ray, out hit);
+    }
+}
